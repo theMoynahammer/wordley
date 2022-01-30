867161490ad328bacd29d120226e20ed6f5124e4
Connor Moynahan <connormoynahan@julias-mbp.lan>
2022-01-30 15:06:49 -0500

    saving

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <iframe
+        title="Wordle"
+        src="https://www.powerlanguage.co.uk/wordle"
+        style={{
+          position: "fixed",
+          top: "0",
+          left: "0",
+          bottom: "0",
+          right: "0",
+          width: "100%",
+          height: "100%",
+          border: "none",
+          margin: "0",
+          padding: "0",
+          overflow: "hidden",
+          zIndex: "999999",
+        }}
+      >
+        Your browser doesn't support iframes
+      </iframe>
     </div>
   );
 }
